eace65bd4497e19013e883da5b533262a70bfc86
bhagavath15 <bhagavath.sbkgmail.com>
2023-06-09 17:17:50 +0530

    cors error

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ await client.connect();
 console.log('mongo is connected!!');
 
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 
 // Use bodyParser middleware to parse JSON in request body
